242b5d2901178e3de76b5a7131e2ab77202946e9
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2023-10-26 16:57:37 +0000

    Pending changes exported from your codespace

---
+++ index.js
@@ -0,0 +1,41 @@
+const david = {
+    rank: 1,
+    percentage: 0
+}
+const muhammid = {
+    rank: 1,
+    percentage: 0
+}
+const scott = {
+    rank: 1,
+    percentage: 0
+}
+const neil = {
+    rank: 1,
+    percentage: 0
+}
+const ryan = {
+    rank: 1,
+    percentage: 0
+}
+const kenyatta = {
+    rank: 1,
+    percentage: 0
+}
+const andrew = {
+    rank: 1,
+    percentage: 0
+}
+const rakib = {
+    rank: 1,
+    percentage: 0
+}
+const andrae = {
+    rank: 1,
+    percentage: 0
+}
+
+const aj = {
+    rank: 1,
+    percentage: 0
+}
